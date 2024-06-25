c8c70af9093726b5a226e445478a7f5147d8c2e6
Siri S <52484274+sirigowda204@users.noreply.github.com>
2024-06-25 16:28:09 -0400

    Completed

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,39 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
+// Your code here along with comments explaining your approach in three sentences only
+
+// Question: Find Missing Number from a Sorted Array.
+class Solution {
+  public int findMissing(int[] nums) {
+    //Edge cases.
+    if(nums.length == 0 || nums == null) return -1;
+
+    int low = 0;
+    int high = nums.length-1;
+
+    while(low<=high) {
+        int mid = low + (high-low)/2;
+        //Check if element at mid is the one missing.
+        if(nums[mid]!=mid+1) {
+            //Check if the element before mid is not satisfying the condition as well.
+            if(nums[mid-1]!=mid) {
+                high = mid -1;
+            }else {
+                return mid+1;
+            }
+        }else {
+            low = mid + 1;
+        }
+    }
+    return low+1;
+  }
+
+  public static void main(String args[]) {
+    int nums[] = {1,2,3,4,6,7,8,9};
+    int result = findMissing(nums);
+    System.out.println("Missing number: "+result);
+  }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,82 @@
+// Time Complexity : Insert, extractMin - O(log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
+
+// Your code here along with comments explaining your approach in three sentences only
+class MinHeap {
+  int[] minHeap;
+  int capapcity;
+  int size;
+
+  //Constructor
+  MinHeap(int capapcity) {
+    this.capapcity = capapcity;
+    this.size = 0;
+    minHeap = new int[capacity];
+  }
+
+  // Getting parent, left and right child if given an index.
+  int getParent(int pos) return pos/2;
+  int getLeftChild(int pos) return 2*pos;
+  int getRightChild(int pos) return 2*pos+1;
+
+  // Checking if an index is a left node.
+  boolean isLeafChild(int pos) return pos > (size/2) && pos < size;
+
+  // Swapping two elements
+  void swap(int first, int second) {
+    int temp = minHeap[first];
+    minHeap[first] = minHeap[second];
+    minHeap[second] = temp;
+  }
+
+  // Inserting an element into the minHeap
+  void insert(int element) {
+    if(size>=capapcity) throw new IllegalStateException("Heap is full");
+    minHeap[size] = element;
+    int currentSize = size;
+    size++;
+    while(currentSize!=0 && minHeap[currentSize] < minHeap[getParent(currentSize)]) {
+      swap(currentSize, getParent(currentSize));
+      currentSize = getParent(currentSize);
+    }
+  }
+
+  // Extracting the first value from a minHeap
+  int extractMin() {
+    if(size == 0) throw new IllegalStateException("Heap is Empty");
+    int result = minHeap[0];
+    minHeap[0] = minHeap[--size];
+    heapify(0);
+    return result;
+  }
+
+  // To make sure the heap is a minHeap.
+  void heapify(int pos) {
+    if(!isLeafChild(pos)) {
+      int left = getLeftChild(pos);
+      int right = getRightChild(pos);
+      int smallest = pos;
+      if(left<size && minHeap[left]<minHeap[smallest]) smallest = left;
+      if(right<size && minHeap[right]<minHeap[smallest]) smallest = right;
+      if(smallest!=pos) {
+        swap(smallest, pos);
+        heapify(smallest);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    MinHeap minHeap = new MinHeap(10);
+    minHeap.insert(10);
+    minHeap.insert(15);
+    minHeap.insert(20);
+    minHeap.insert(17);
+
+    System.out.println(minHeap.extractMin()); // 10
+    System.out.println(minHeap.extractMin()); // 15
+  }
+
+}
